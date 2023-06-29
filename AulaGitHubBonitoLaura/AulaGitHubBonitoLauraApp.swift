8db91126a353d2747aed4187a873d2a2c538afca
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-06-29 16:19:14 -0300

    Initial Commit

---
+++ AulaGitHubBonitoLaura/AulaGitHubBonitoLauraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaGitHubBonitoLauraApp.swift
+//  AulaGitHubBonitoLaura
+//
+//  Created by Gabriel Vicentin Negro on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AulaGitHubBonitoLauraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
